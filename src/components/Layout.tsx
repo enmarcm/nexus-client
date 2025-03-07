a35a4a1a17909269dc8ef81295e8c6b84a1d239f
enmarcm <theenmanuel123@gmail.com>
2025-03-07 02:23:34 -0400

    feat(Layout): create Layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Layout = ({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className="bg-backgroundWhite min-h-screen flex flex-col">
+      <div className="flex justify-between items-center h-7 my-2 px-8 py-4">
+        <div>
+          <h1 className="text-lg text-gray-700 font-semibold">{title}</h1>
+        </div>
+        <div>Seleccion de idioma</div>
+      </div>
+
+      <div className="flex-1 overflow-y-auto px-8 py-4">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
