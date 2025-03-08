13b329691f0d9507358b658ac89d595d7b9c2797
enmarcm <theenmanuel123@gmail.com>
2025-03-08 17:43:34 -0400

    feat(Home): add new charts to the home page

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,22 +1,27 @@
 import CardNotification from "../components/CardNotification";
+import ContainerGraphics from "../components/Graphics/ContainerGraphics";
 import Layout from "../components/Layout";
+import dataGraphics from "../data/graphics.json";
 
 const Home = () => {
   return (
     <Layout title={"Home"}>
       <section className="flex flex-col w-full h-full gap-8">
-        <div className="flex justify-around">
+        <div className="flex justify-between">
           <CardNotification />
           <CardNotification />
           <CardNotification />
           <CardNotification />
         </div>
 
-        <div className="flex min-h-80">
+        <div className="flex h-80 gap-4 overflow-hidden">
           <div className="w-7/12 h-full min-h-full">
+            <ContainerGraphics configGraphics={dataGraphics.firstHomeGraphic} />
           </div>
 
-          <div className="w-5/12 h-full min-h-full"></div>
+          <div className="w-5/12 h-full min-h-full">
+            <ContainerGraphics configGraphics={dataGraphics.secondHomeGraphic} />
+          </div>
         </div>
       </section>
     </Layout>
