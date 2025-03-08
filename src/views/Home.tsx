43a99051b0eed02052f952808ef4ca240f2f7d2f
enmarcm <theenmanuel123@gmail.com>
2025-03-07 20:02:43 -0400

    feat(Tremor): add Tremor Framework UI to use

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,19 +2,21 @@ import CardNotification from "../components/CardNotification";
 import Layout from "../components/Layout";
 
 const Home = () => {
- 
   return (
     <Layout title={"Home"}>
       <section className="flex flex-col w-full h-full gap-8">
         <div className="flex justify-around">
-        <CardNotification/>
-        <CardNotification/>
-        <CardNotification/>
-        <CardNotification/>
+          <CardNotification />
+          <CardNotification />
+          <CardNotification />
+          <CardNotification />
+        </div>
 
-          
+        <div className="flex min-h-80">
+          <div className="w-7/12 h-full min-h-full">
+          </div>
 
-
+          <div className="w-5/12 h-full min-h-full"></div>
         </div>
       </section>
     </Layout>
